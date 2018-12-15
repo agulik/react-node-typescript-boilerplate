6f62bbc64ad9cd33bffc9594247ac76b02a4a5f9
Andrew Gulik <andrewgulik@gmail.com>
2018-12-15 16:34:07 -0500

    chore: add some prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import express from 'express'
-import bodyParser from "body-parser";
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
 
 const app = express()
 
 // Express configuration
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 /**
  * Primary app routes.
  */
-app.get('/', function (req, res) {
-  res.send('hello world')
+app.get("/", function(req, res) {
+  res.send("hello world")
 })
 
-app.listen(8080, () => console.log('Server is listening on port 8080'))
+app.listen(8080, () => console.log("Server is listening on port 8080"))
