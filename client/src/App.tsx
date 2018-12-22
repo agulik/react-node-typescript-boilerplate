f17d1f0a27a99d66ada9b27fd22f5e664933979f
Andrew Gulik <andrewgulik@gmail.com>
2018-12-21 20:22:39 -0500

    feat: starting to build out client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,19 @@
 import * as React from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
 import "./App.css"
+import Home from "./containers/home"
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <Router>
+          <Switch>
+            <Route exact={true} path="/" component={Home} />
+          </Switch>
+        </Router>
+      </main>
     )
   }
 }
---
+++ client/src/containers/home/home.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+class Home extends React.PureComponent {
+  render() {
+    return <div>Home 🐒</div>
+  }
+}
+
+export default Home
