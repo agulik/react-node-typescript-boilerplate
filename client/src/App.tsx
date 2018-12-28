86457449411424b025a10af26c3f943497442d7d
Andrew Gulik <andrewgulik@gmail.com>
2018-12-27 19:00:35 -0500

    chore: rm some things

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,11 @@
 import React, { Component } from "react"
 import "./App.css"
 
-const logo = "./logo.svg"
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
