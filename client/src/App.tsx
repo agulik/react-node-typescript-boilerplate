c6d6e10b48f545fc8fc306ae17c8ab28be040241
Andrew Gulik <andrewgulik@gmail.com>
2018-12-28 12:19:51 -0500

    chore: refactor client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,12 @@
 import React, { Component } from "react"
 import "./App.css"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Home Page
-          </a>
-        </header>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Home Page</header>
+    </div>
+  )
 }
 
 export default App
