edd6a773a7b405cd82a0bc9e10b07f9a73a0092d
Andrew Gulik <andrewgulik@gmail.com>
2018-12-29 18:21:36 -0500

    feat: work on client and draft out home container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react"
+
 import "./App.css"
+import Home from "./containers/home"
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">Home Page</header>
+      <Home />
     </div>
   )
 }
